f14270d00e43ac0c388e8684aebf5e4ff6314e25
Evan Sterling <Evan Sterling>
2016-07-01 04:51:05 +0300

    basic app routes

---
+++ app/signup/signup-controller.js
@@ -0,0 +1,6 @@
+(function(){
+	angular.module('TimeWaste')
+.controller('SignupController', ['$scope', '$state', function($scope, $state){
+		
+	}]);
+}());
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('./app/sass/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('./app/css'));
+});
+gulp.task('watch', function() {
+	gulp.watch('./app/sass/*.sass', ['sass']);
+})
+
+gulp.task('default', ['sass', 'watch']);
