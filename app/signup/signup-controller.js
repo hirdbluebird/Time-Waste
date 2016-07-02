e14e70119fb7ce8541ce99df2e22a5c0a25966dd
Evan Sterling <Evan Sterling>
2016-07-02 05:56:25 +0300

    login user done

--- app/signup/signup-controller.js
+++ app/signup/signup-controller.js
@@ -1,6 +1,12 @@
 (function(){
 	angular.module('TimeWaste')
-.controller('SignupController', ['$scope', '$state', function($scope, $state){
-		
+.controller('SignupController', ['$scope', '$state', '$http', function($scope, $state, $http){
+		$scope.createUser = function(){
+			$http.post('api/user/signUp', $scope.newUser).success(function(responce){
+
+			}).error(function(error){
+				console.log(error);
+			})
+		}
 	}]);
 }());
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var app = express();
+var authenticationController = require('./server/controllers/authentication-controller');
 
 mongoose.connect('mongodb://localhost:27017/time-waste');
 
+app.use(bodyParser.json());
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
@@ -13,6 +15,12 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'))
 app.get('/', function(req, res) {
 	res.sendfile('index.html');
 });
+
+//Athentication
+app.post('/api/user/signup', authenticationController.signup);
+app.post('/api/user/login', authenticationController.login);
+
+
 
 app.listen('3000', function() {
 	console.log('Listening for local host 3000');
