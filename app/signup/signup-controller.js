2233e875f109c375b0cc442df7a91a6da09f1c02
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-07-03 06:54:48 +0300

    test

--- app/signup/signup-controller.js
+++ app/signup/signup-controller.js
@@ -2,7 +2,7 @@
 	angular.module('TimeWaste')
 .controller('SignupController', ['$scope', '$state', '$http', function($scope, $state, $http){
 		$scope.createUser = function(){
-			$http.post('api/user/signUp', $scope.newUser).success(function(responce){
+			$http.post('api/user/signUp', $scope.newUser).success(function(responce) {
 
 			}).error(function(error){
 				console.log(error);
